6219fdf44f52019375917e7acf32dd45903964aa
sim <shimhyerin2@gmail.com>
2025-04-04 16:50:09 +0900

    파이썬5주차과제_202310463심혜린
    구구단 출력 프로그램

---
+++ 파이썬5주차과제_202310463심혜린.py
@@ -0,0 +1,13 @@
+for k in range (2, 10):
+    guguline = print(" #  %d단  #" % k, end=" ")
+
+
+print()
+
+for i in range (1, 10):
+    for j in range (2, 10):
+     
+        print("%2dX %2d= %2d" % (j, i, i*j), end=" ")
+   
+        
+    print()
