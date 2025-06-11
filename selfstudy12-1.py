dabc6f3b2f7d031e995ee530c6e8dcee5e43f9c2
sim <shimhyerin2@gmail.com>
2025-06-11 13:33:15 +0900

    week 11 과제

---
+++ selfstudy12-1.py
@@ -0,0 +1,32 @@
+class car :
+    color = ""
+    speed = 0
+
+    def upSpeed(self, value) :
+        self.speed += value
+        if (self.speed > 150) :
+            self.speed = 150
+
+    def downSpeed(self, value) :
+        self.speed -= value
+
+myCar1 = car()
+myCar1.color = "빨강"
+myCar1.speed = 0
+
+myCar2 = car()
+myCar2.color = "파랑"
+myCar2.speed = 0
+
+myCar3 = car()
+myCar3.color = "노랑"
+myCar3.speed = 0
+
+myCar1.upSpeed(200)
+print("자동차1의 색상은 %s이며, 현재 속도는 %dkm입니다." % (myCar1.color, myCar1.speed))
+
+myCar2.upSpeed(60)
+print("자동차2의 색상은 %s이며, 현재 속도는 %dkm입니다." % (myCar2.color, myCar2.speed))
+
+myCar3.upSpeed(0)
+print("자동차3의 색상은 %s이며, 현재 속도는 %dkm입니다." % (myCar3.color, myCar3.speed))
